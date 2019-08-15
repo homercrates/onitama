b34a54574aad1fadb4f7392ef2f5b574f901b76d
smw mc <smeredithwilliams@gmail.com>
2019-08-15 16:24:12 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="redPlayerCards">
+        red player cards
+      </div>
+      <div className="middleBoard">
+        <div className="limboCard">
+          limbo card
+        </div>
+        <div className="board">
+          board here
+        </div>
+      </div>
+      <div className="bluePlayerCards">
+        blue player cards
+      </div>
     </div>
   );
 }
