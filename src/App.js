b3c132938eda17121621fd83fe2318d30f6bd0c5
smw mc <smeredithwilliams@gmail.com>
2019-09-10 21:03:15 -0700

    barebones restart

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+
+
+const App = () => {
+    return (
+        <React.StrictMode>
+            <div>
+                App
+      </div>
+        </React.StrictMode>
+    );
+};
+
+render(<App />, document.getElementById("root"));
