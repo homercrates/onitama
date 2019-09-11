adf155af5b3bea73cafec401d990e1463df247c3
smw mc <smeredithwilliams@gmail.com>
2019-09-10 22:37:31 -0700

    Board is set up.  It works now

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 
+import GameView from './view/GameView';
 
 const App = () => {
     return (
         <React.StrictMode>
             <div>
-                App
-      </div>
+                <GameView />
+            </div>
         </React.StrictMode>
     );
 };
