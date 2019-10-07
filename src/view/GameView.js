9a6524cafff35c5e710837a16e85f5ede65ddeda
smw mc <smeredithwilliams@gmail.com>
2019-10-06 20:30:14 -0700

    gameView just board there now errased bug notes

--- src/view/GameView.js
+++ src/view/GameView.js
@@ -6,16 +6,7 @@ const GameView = () => {
     return (
 
         <div>
-            <hr />
-            <p>NEW BUG I am actually reversing the cards right its just not posting to
-                state when i need it.  its suppsed to tave reverse and setFlippedValue
-                but that is delayed about a turn later it shows up
-
-            </p>
-            <hr />
-            <p>switchCard is hard coded to red. we need to make it dynamicaly poerate on redHand or blueHand depending on turn</p>
-            <p> reverse values into blues hand upside down.</p>
-            <p>and then create a win condition and check it then framework built</p>
+            Onitama
             <hr />
             <Board />
         </div>
